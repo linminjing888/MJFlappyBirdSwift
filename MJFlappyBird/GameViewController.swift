dbce37ce47b7bc1bc9dba3fa1fc0834d3823480d
YXCZ <minjing@mc2050.com>
2017-09-05 18:38:12 +0800

    gif 图

--- MJFlappyBird/GameViewController.swift
+++ MJFlappyBird/GameViewController.swift
@@ -16,14 +16,11 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+
+            let scene = GameScene(size: self.view.bounds.size)
                 
-                // Present the scene
-                view.presentScene(scene)
-            }
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
